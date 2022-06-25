944086b23309ada5515c3c3bf1a3047640734051
keysquivered <936095897@qq.com>
2022-06-25 15:28:57 +0800

    远程函数：添加描述

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,12 @@
 package handler
 
-// handler must define in advance
+// handler 服务端 的远程函数列表
 
 const (
 	RpcServiceName = "RpcService"
 )
 
+// RpcService 添加远程函数 的业务逻辑
 type RpcService struct{}
 
 func (s *RpcService) Show(req string, res *string) (err error) {
